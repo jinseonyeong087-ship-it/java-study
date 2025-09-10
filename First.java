db59775de1f537d086fa1d154aa4bd04ee9c85ae
jinseonyeong087-ship-it <jinseonyeong087@gmail.com>
2025-09-10 11:56:33 +0900

    9/10

---
+++ First.java
@@ -0,0 +1,19 @@
+public class First {
+
+    public static void main(String[] args) {
+
+        // int value = 0;
+
+        // int result = value + 10;
+
+        // System.out.println(result);
+
+        int hour = 3;
+        int minute = 5;
+        System.out.println(hour + "시간 " + minute +"분");
+
+        int totalMinute = (hour*60) + minute;
+        System.out.println("총 "+ totalMinute + "분");
+    }
+
+}
---
+++ Terminology.md
@@ -0,0 +1,12 @@
+# 용어정리
+## javac 
+ Java Compiler (자바 컴파일러 - .java 소스 파일을 컴파일해서 .class 바이트코드 파일을 만드는 명령어)
+
+## method
+클래스 안에 정의된 동작(기능). 객체가 할 수 있는 “동사(행동)”를 코드로 작성
+
+## Path 환경 변수
+운영체제(OS) 가 프로그램 실행 파일을 찾는 검색 경로
+
+## 예약어
+Java 언어에서 이미 특정 의미/용도로 정해진 단어 (class, public, static, void, int, if, else, for, while 등)
