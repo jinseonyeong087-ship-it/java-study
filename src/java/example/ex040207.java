233ea4562c5cc559a628b792bdcb6401123013cb
jinseonyeong087-ship-it <jinseonyeong087@gmail.com>
2025-09-16 10:41:51 +0900

    9/16

--- src/java/example/ex040207.java
+++ src/java/example/ex040207.java
@@ -7,8 +7,13 @@ public class ex040207 {
         Scanner sc = new Scanner(System.in);
 
         while (run) {
-            System.out.println("------------------------");
+            System.out.println("-------------------------------------");
             System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+            System.out.println("-------------------------------------");
+            System.out.print("선택> ");
+            
+
         }
+        System.out.println("프로그램 종료");
     }
 }
---
+++ src/java/individual/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+    // 정적 변수: 모든 Student 객체가 공유
+    private static int count = 0;
+
+    private String name;
+
+    // 생성자
+    public Student(String name) {
+        this.name = name;
+        count++; // 학생 객체가 생성될 때마다 count 증가
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // 정적 메서드: 객체 생성 없이도 호출 가능
+    public static int getCount() {
+        return count;
+    }
+}
+
+
+
