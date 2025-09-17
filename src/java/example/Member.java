c3bc8c6dd6cec51c6978b0b53ea5c50446df42d7
jinseonyeong087-ship-it <jinseonyeong087@gmail.com>
2025-09-17 15:53:59 +0900

    9/17

--- src/java/example/Member.java
+++ src/java/example/Member.java
@@ -1,26 +1,10 @@
 // public class ex060202 {     // ✅ 클래스 선언 (외부 클래스)
     
-    // public class Member {   // ✅ 클래스 선언 (내부 클래스)
-    //     String name;        // 필드
-    //     String id;          // 필드
-    //     String password;    // 필드
-    //     int age;            // 필드
-    // }
-// }
-
-
-public class Member {
-    String name;
-    String id;
-
-    // 생성자 (이름 + 아이디 초기화)
-    Member(String name, String id) {
-        this.name = name;
-        this.id = id;
+    public class Member {   // ✅ 클래스 선언 (내부 클래스)
+        String name;        // 필드
+        String id;          // 필드
+        String password;    // 필드
+        int age;            // 필드
     }
 
-    // 메서드: 정보 출력
-    void printInfo() {
-        System.out.println("이름: " + name + ", 아이디: " + id);
-    }
-}
+
--- src/java/example/ex060302.java
+++ src/java/example/ex060302.java
@@ -6,3 +6,25 @@ public class ex060302 {
         user1.printInfo();  // 생성된 값 확인
     }
 }
+
+
+ class Member {
+    String name;
+    String id;
+    public int age;
+
+    // 생성자 (이름 + 아이디 초기화)
+    Member(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Member() {
+        //TODO Auto-generated constructor stub
+    }
+
+    // 메서드: 정보 출력
+    void printInfo() {
+        System.out.println("이름: " + name + ", 아이디: " + id);
+    }
+}
