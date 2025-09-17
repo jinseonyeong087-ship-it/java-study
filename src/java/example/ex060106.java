4d910cf599d856241971de62ac99f200613b847b
jinseonyeong087-ship-it <jinseonyeong087@gmail.com>
2025-09-17 14:25:07 +0900

    9/17

---
+++ src/java/example/ex060106.java
@@ -0,0 +1,13 @@
+public class ex060106 {
+    
+   
+        public class Member {
+        String name;                    //필드
+
+        Member(String name) {}          //생성자
+
+        void setName(String name) {}    //메소드
+            
+        }
+    }
+
