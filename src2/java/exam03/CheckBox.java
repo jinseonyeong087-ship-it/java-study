f6273a14daaa795ef02bd5d65e006cb58b4d64d8
jinseonyeong087 <jinseonyeong087@gmail.com>
2025-09-22 01:46:37 +0900

    9/22

---
+++ src2/java/exam03/CheckBox.java
@@ -0,0 +1,20 @@
+package src2.java.exam03;
+
+public class CheckBox {
+    OnSelectListener listener;
+
+    void setOnSelectListener(OnSelectListener listener) {
+        this.listener = listener;
+    }
+
+    void select() {
+        listener.onSelect();
+    }
+
+    static interface OnSelectListener {
+        void onSelect();
+    }
+
+}
+    
+
