9e009301dc81e59420c56f625282df4703bb270e
jinseonyeong087-ship-it <jinseonyeong087@gmail.com>
2025-09-22 16:46:13 +0900

    9/22

---
+++ src2/java/StringConverExample.java
@@ -0,0 +1,11 @@
+package src2.java;
+
+public class StringConverExample {
+    public static void main(String[] args) {
+        String strData1 = "200";
+        int intData1 = Integer.parseInt(strData1);
+
+        int intData2 = 150;
+        String strData2 = String.valueOf(intData2);
+    }
+}
